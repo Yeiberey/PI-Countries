beaf26049dc76ca082cea80b2cba45106146afb0
yeiberey <yeiberey@gmail.com>
2023-01-27 19:57:43 -0500

    dbApiUpdated

--- api/src/db.js
+++ api/src/db.js
@@ -1,26 +1,53 @@
-require('dotenv').config();
-const { Sequelize, Op } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, } = process.env;
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-  logging: false, /* console.log, */ // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  /* define: {//detener la pluralización global
-    freezeTableName: true
-  } */
-  
-});
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : // new Sequelize(
+      //     "postgresql://RRyzPcfGxULksUDsvWFTfGGjtFXBCrAe:EDFdRlQWWNFltJkKGLPOTMAysWhFlvqK@db.thin.dev/a81cde44-137b-4180-8f44-6e27416e60dc",
+      //     { logging: false, native: false }
+      //   );
+      new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -28,27 +55,36 @@ modelDefiners.forEach((model, i) => {
   // if(i === 2){
   //   return model(sequelize,sequelize.models.Country,sequelize.models.Activity)
   // }
-  return model(sequelize)
+  return model(sequelize);
 });
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Country, Activity } = sequelize.models;
 //const Countries_Activities = sequelize.define('Countries_Activities', {}, { timestamps: false });
-console.log(sequelize.models)
+console.log(sequelize.models);
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
-Country.belongsToMany(Activity, { through: 'country_activity', /*as:'activity' Countries_Activities, foreignKey: 'country_id' */});
-Activity.belongsToMany(Country, { through: 'country_activity', /*as:'country' Countries_Activities, foreignKey:  'activity_id' */});
+Country.belongsToMany(Activity, {
+  through:
+    "country_activity" /*as:'activity' Countries_Activities, foreignKey: 'country_id' */,
+});
+Activity.belongsToMany(Country, {
+  through:
+    "country_activity" /*as:'country' Countries_Activities, foreignKey:  'activity_id' */,
+});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
   Op,
 };
